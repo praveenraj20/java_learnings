38f7277e7dc2b842fdeee50faf13c17d8e4827f3
praveenraj20 <92568736+praveenraj20@users.noreply.github.com>
2024-09-25 10:20:33 +0530

    java8_Streams

--- java/src/streams/CharFrequency.java
+++ java/src/streams/CharFrequency.java
@@ -1,4 +1,4 @@
-package iq;
+package streams;
 
 import java.util.Map;
 import java.util.function.Function;
--- java/src/streams/SquareEvenNos.java
+++ java/src/streams/SquareEvenNos.java
@@ -1,4 +1,4 @@
-package iq;
+package streams;
 
 import java.util.Arrays;
 import java.util.List;

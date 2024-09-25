550640248435b783ae6a6bba6ec6fea6a1773f85
praveenraj20 <92568736+praveenraj20@users.noreply.github.com>
2024-09-25 10:24:20 +0530

    String

--- java/src/string/RemoveDuplicatesInString.java
+++ java/src/string/RemoveDuplicatesInString.java
@@ -9,16 +9,14 @@ public class RemoveDuplicatesInString {
     public static  void main(String[] args){
         List<String> listOfStrings = Arrays.asList("Apple","Orange","Grapes","Amla","Grapes");
 
-        List<String> x = listOfStrings.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()))
-                .entrySet().stream().filter(e -> e.getValue() == 1).map(Map.Entry::getKey).collect(Collectors.toList());
-        System.out.println(x);
-//        List<String> nonDuplicate = listOfStrings.stream()
-//                .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
-//                .entrySet().stream()
-//                .filter(e -> e.getValue() == 1)
-//                .map(Map.Entry::getKey)
-//                .collect(Collectors.toList());
-//        System.out.println(nonDuplicate);
+
+        List<String> nonDuplicate = listOfStrings.stream()
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
+                .entrySet().stream()
+                .filter(e -> e.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        System.out.println(nonDuplicate);
 
     }
 }

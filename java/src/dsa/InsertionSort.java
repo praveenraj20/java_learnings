63d6c5ef14291bc9118c08d2ee62bc3eeecc39e5
praveenraj20 <92568736+praveenraj20@users.noreply.github.com>
2024-10-22 15:04:55 +0530

    InsertionSort

--- java/src/dsa/InsertionSort.java
+++ java/src/dsa/InsertionSort.java
@@ -9,7 +9,7 @@ public class InsertionSort {
             int key = arr[i];
             int j = i-1;
             while (j>=0 && arr[j]>key){
-                arr[j+1] =arr[j];
+                arr[j+1] =arr[j];         // shifting the element towards right
                 j--;
             }
             arr[j+1]=key;

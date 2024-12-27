224cb4c243537dc763153744619e11e83df634de
praveenraj20 <92568736+praveenraj20@users.noreply.github.com>
2024-12-27 18:55:35 +0530

    SecLargest with TimeComplexity and Space Complexity

--- coding/src/gfg/SecLargest.java
+++ coding/src/gfg/SecLargest.java
@@ -1,24 +1,89 @@
 package gfg;
 
+import java.util.Arrays;
+
 /**
  * DAY-1
  **/
 public class SecLargest {
     public static void main(String[] args) {
-        int num[] = {18, 75, 45, 30, 29, 11};
-        int length = num.length;
+        int[] arr = {40, 76, 40, 76, 45, 43, 23, 9, 67};
+        int length = arr.length;
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length - 1; j++) {
-                if (num[i] < num[j]) {
-                    int tmp = num[i];
-                    num[i] = num[j];
-                    num[j] = tmp;
+                if (arr[i] < arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
                 }
             }
         }
         for (int k = 0; k < length; k++) {
-            System.out.println(num[k]);
+            System.out.println(arr[k]);
         }
-        System.out.println("Second Largest number is :" + num[length - 2]);
+        System.out.println("My Approach: " + arr[length - 2]);
+
+        System.out.println("secLargestNoApproachOne: " + secLargestNoApproachOne(arr));
+
+        System.out.println("secLargestNoApproachTwo: " + secLargestNoApproachTwo(arr));
+
+        System.out.println("secLargestNoApproachThree: " + secLargestNoApproachThree(arr));
+    }
+
+    static int secLargestNoApproachOne(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] != arr[n - 1]) {
+                return arr[i];
+            }
+        }
+        return -1;
+        // t.c = O(nlogn)
+        // s.c = O(1);
+    }
+
+    static int secLargestNoApproachTwo(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        int largest = Integer.MIN_VALUE;
+        int secLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > secLargest && arr[i] != largest) {
+                secLargest = arr[i];
+            }
+        }
+        return secLargest;
+        // t.c = O(2n)
+        // s.c =O(1)
+    }
+
+    static int secLargestNoApproachThree(int[] arr) {
+        int n = arr.length;
+        int largest = -1;
+        int secLargest = -1;
+
+        if (n < 2) {
+            return -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secLargest && arr[i] < largest) {
+                secLargest = arr[i];
+            }
+        }
+        return secLargest;
+        // t.c = O(n)
+        // s.c = O(1)
     }
 }
+

512ce77b14dbe152df90b92d54ce0b2d0ad3e180
praveenraj20 <92568736+praveenraj20@users.noreply.github.com>
2024-12-28 22:54:47 +0530

    PushZerosToEndInArray

---
+++ coding/src/gfg/PushZerosToEndInArray.java
@@ -0,0 +1,68 @@
+package gfg;
+
+/**
+ * DAY-2
+ **/
+
+public class PushZerosToEndInArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 0, 0, 4};
+        int n = arr.length;
+        int[] temp = new int[n];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                temp[j++] = arr[i];
+            }
+        }
+        while (j < n) {
+            temp[j++] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+        for (int num : arr) {
+            System.out.print(" " + num);
+        }
+        System.out.println();
+        pushZerosToEndSecondApproach(arr);
+        for (int num : arr) {
+            System.out.print(" " + num);
+        }
+        System.out.println();
+        pushZerosToEndThirdApproach(arr);
+        for (int num : arr) {
+            System.out.print(" " + num);
+        }
+    }
+
+    static void pushZerosToEndSecondApproach(int[] arr) {
+        System.out.println("Second Approach :");
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i];
+            }
+        }
+        while (count < arr.length) {
+            arr[count++] = 0;
+        }
+        // t.c = O(2n) ~ O(n)
+        // S.c  = O(1)
+    }
+
+    static void pushZerosToEndThirdApproach(int[] arr) {
+        System.out.println("Third Approach: ");
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = arr[i];
+                count++;
+            }
+        }
+    }
+    // t.c = O(2n) ~ O(n)
+    // S.c  = O(1)
+}

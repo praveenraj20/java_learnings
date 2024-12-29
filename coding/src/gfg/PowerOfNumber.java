ad2a1a2865ee313516aa5647fb8864b1c4d19f71
praveenraj20 <92568736+praveenraj20@users.noreply.github.com>
2024-12-29 19:18:49 +0530

    powerOfNumber ApproachTwo

--- coding/src/gfg/PowerOfNumber.java
+++ coding/src/gfg/PowerOfNumber.java
@@ -1,14 +1,24 @@
 package gfg;
+
 public class PowerOfNumber {
     public static void main(String[] args) {
-        int number  = 3;
-        int pwr = 2;
-        int n = number ;
+        int number = 3;
+        int pwr = 4;
+        int n = number;
         int temp;
-        for(int i=1;i<pwr;i++){
-            temp = n*number;
-            n=temp;
+        for (int i = 1; i < pwr; i++) {
+            temp = n * number;
+            n = temp;
         }
         System.out.println(n);
+        powerOfNumberApproachTwo(number, pwr);
+    }
+
+    private static void powerOfNumberApproachTwo(int number, int pwr) {
+        int result = 1;
+        for (int i = 0; i < pwr; i++) {
+            result *= number;
+        }
+        System.out.println(result);
     }
 }

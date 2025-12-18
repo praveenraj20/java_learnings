0ebc83a1f2e4bf0dae0e1bc299445f3ec3f57d95
Praveenraj Rajasekaran <rpraveenraj@athenahealth.com>
2025-12-18 22:22:05 +0530

    Longest Common Prefix

---
+++ coding/src/neetcode/arrays/LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+package neetcode.arrays;
+
+/**
+ * Input: strs = ["dance","dag","danger","damage"]
+ * Output: "da"
+ */
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strs = {"dance", "dag", "danger", "damage"};
+        System.out.println("Longest Common Prefix: "+longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+    /**
+     * Time Complexity = O(n x m)
+     * Space Complexity = O(1)
+     * **/
+}
+

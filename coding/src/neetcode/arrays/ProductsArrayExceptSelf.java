12accf15f51bb507cf0c9cedfa6494ab01da3d45
Praveenraj Rajasekaran <rpraveenraj@athenahealth.com>
2026-01-09 13:31:56 +0530

    ProductsArrayExceptSelf

---
+++ coding/src/neetcode/arrays/ProductsArrayExceptSelf.java
@@ -0,0 +1,41 @@
+package neetcode.arrays;
+
+import java.util.Arrays;
+
+/**
+ * Input: nums = [1,2,4,6]
+ * Output: [48,24,12,8]
+ **/
+public class ProductsArrayExceptSelf {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 4, 6};
+        int[] result = productExceptSelf(num);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+
+        left[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = left[i] * right[i];
+        }
+        return result;
+    }
+}
+
+/**
+ * Time Complexity = O(n)
+ * Space Complexity = O(n)
+ **/
